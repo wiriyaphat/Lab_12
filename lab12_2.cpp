1448a5ea8f6b3c2c71b84f0a61e99194a7dc4073
Thiw <wiriyaphat6789@gmail.com>
2025-12-30 14:37:11 +0700

    Lab12_2 complete

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
+#include <ctime>
 using namespace std;
 
+long long fibonacci (long long);
+
 int main(){
-    int x, y;
-    cin >> x >> y;
-    cout << gcd(x,y);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+long long fibonacci (long long a){
+    if (a==0) return 0;
+    else if (a==1) return 1;
+    else {return fibonacci(a-1) + fibonacci(a-2);}
 }
